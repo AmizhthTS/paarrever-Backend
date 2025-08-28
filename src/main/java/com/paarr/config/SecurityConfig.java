e25e904a0962f44bb6e37851cdf9ad95b72501ff
Arun Pandiyan <130133258+arunvijaygithub@users.noreply.github.com>
2025-08-28 18:47:36 +0530

    allow options in security config

--- src/main/java/com/paarr/config/SecurityConfig.java
+++ src/main/java/com/paarr/config/SecurityConfig.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -28,8 +29,11 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
         	.cors(cors -> {})
+        	
             .authorizeHttpRequests(auth -> auth
+            		.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                     .requestMatchers("/auth/**").permitAll()
+                   
                     .requestMatchers("/category/**","/contact/**","/subcategory/**","/store/**","/newsletter/**").permitAll() 
                     .anyRequest().authenticated()
             )
